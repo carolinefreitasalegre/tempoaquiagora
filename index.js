ce60e5f7e13896952018a40f403d1a838c23d101
Caroline <113471098+carolinefreitasalegre@users.noreply.github.com>
2023-02-11 18:11:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const chave = "d9ef783dd77b9937315748849c22b81b"
 const button = document.getElementById('button')
-
 const input = document.getElementById('input')
+const dataHora = document.getElementById('dataHora')
 const cidadeSpan = document.getElementById('cidadeSpan')
 const horarioSpan = document.getElementById('horarioSpan')
 const temperatura = document.getElementById('temperatura')
 const ceu = document.getElementById('ceu')
-const img02 = document.getElementById('img02')
 
 
 function buscar() {
@@ -14,7 +13,17 @@ function buscar() {
 
   getDataApi()
 }
+// adicionado o evento de clik
+input.addEventListener('keyup', (e)  => {
+  if (e.code === "Enter") {
+    const cidadeSpan = e.target.value
+    getDataApi(cidadeSpan)
 
+  }
+})
+  
+  
+  
 async function getDataApi() {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&units=metric&appid=${chave}&lang=pt_br`
 
@@ -33,11 +42,33 @@ async function getDataApi() {
 
 }
 function loadData(data) {
+  let nowDate = new Date()
+  
 
+
+  dataHora.innerText = dateBuilder(nowDate);
   cidadeSpan.innerText = `${data.name}, ${data.sys.country}`;
   temperatura.innerText = `${parseFloat(data.main.temp.toFixed())}º C`
   ceu.innerText = `${data.weather[0].description}`
-  img02.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 
+  
+  const imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 
+  const img02 = document.getElementById('img02')
+
+  img02.setAttribute('src', imgUrl)
+  
+}
+function dateBuilder(data) {
+  let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+  let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+  let day = days[data.getDay()]; //getDay: 0-6
+  let date = data.getDate();
+  let month = months[data.getMonth()];
+  let year = data.getFullYear();
+  let hours = data.getHours()
+  let min = data.getMinutes()
+  return `${day}, ${date} ${month} ${year}. Às ${hours}h:${min}min`;
+  
 }
